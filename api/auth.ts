17a3c72e03f86149fc71564c835b3c6b74eef6c4
Vorachot Banluepongpan <vorachot.ban@gmail.com>
2025-11-22 19:24:00 +0700

    feat: Update API endpoints to use environment variable for improved configuration

--- api/auth.ts
+++ api/auth.ts
@@ -2,41 +2,50 @@ export async function loginUser(
   username: string,
   password: string,
 ): Promise<Response> {
-  const response = await fetch("http://localhost:8080/users/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_NAMESPACE_MANAGER_URL}/users/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      credentials: "include",
+      body: new URLSearchParams({
+        username,
+        password,
+      }),
     },
-    credentials: "include",
-    body: new URLSearchParams({
-      username,
-      password,
-    }),
-  });
+  );
 
   return response;
 }
 export async function logoutUser(): Promise<Response> {
-  const response = await fetch("http://localhost:8080/users/logout", {
-    method: "GET",
-    credentials: "include",
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_NAMESPACE_MANAGER_URL}/users/logout`,
+    {
+      method: "GET",
+      credentials: "include",
+    },
+  );
 
   return response;
 }
 
 export async function me(): Promise<Response> {
-  const response = await fetch("http://localhost:8080/users/me", {
-    method: "GET",
-    credentials: "include",
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_NAMESPACE_MANAGER_URL}/users/me`,
+    {
+      method: "GET",
+      credentials: "include",
+    },
+  );
 
   return response;
 }
 
 export async function googleAuthWithCH(code: string): Promise<Response> {
   const response = await fetch(
-    `http://localhost:8080/users/accessTokens/${code}`,
+    `${process.env.NEXT_PUBLIC_NAMESPACE_MANAGER_URL}/users/accessTokens/${code}`,
     {
       method: "GET",
       credentials: "include",
